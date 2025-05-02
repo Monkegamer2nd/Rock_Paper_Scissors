383e284ffa02cfa5557bb2bc7a280245b055c172
Monkegamer2nd <talon.harris@papamoacollege.school.nz>
2025-05-02 14:37:56 +1200

    RPS (working, semi-complete)

--- B_01_rps_game_v1.py
+++ B_01_rps_game_v1.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 # Check that users have entered a valid
 # option based on a list
 def string_checker(question, valid_ans=("yes", "no")):
@@ -69,6 +73,27 @@ def int_check(question):
         except ValueError:
             print(error)
 
+# compares user / computer choice and returns
+# result (win / lose / tie)
+def rps_compare(user, comp):
+
+    # If the user and the computer choice is the same, it's a tie
+    if user == comp:
+        result = "tie"
+
+    # There are three ways to win
+    elif user == "paper" and comp == "rock":
+        result = "win"
+    elif user == "scissors" and comp == "paper":
+        result = "win"
+    elif user == "rock" and comp == "scissors":
+        result = "win"
+    # if it's not a win / tie, then it's a loss
+    else:
+        result = "lose"
+
+    return result
+
 
 # Main Routine Starts here
 
@@ -107,7 +132,10 @@ while rounds_played < num_rounds:
         rounds_heading = f"\n💿💿💿 Round {rounds_played + 1} of {num_rounds} 💿💿💿"
 
     print(rounds_heading)
-    print()
+
+    # randomly choose from the rps list (excluding the exit code)
+    comp_choice = random.choice(rps_list[:-1])
+    print("Computer choice", comp_choice)
 
     # get user choice
     user_choice = string_checker("Choose: ", rps_list)
@@ -116,6 +144,9 @@ while rounds_played < num_rounds:
     # if users are in infinite mode, or want to finish a game they can use this to break out of the loop.
     if user_choice == "xxx":
         break
+
+    result = rps_compare(user_choice, comp_choice)
+    print(f"{user_choice} vs {comp_choice}, {result}")
 
     rounds_played += 1
 
